e31ad046321b517f6cabc72e4e715723a903be43
mindland <pvdphong.98@gmail.com>
2021-08-23 23:43:34 +0700

    user login API done

--- backend/repositories/userRepository.js
+++ backend/repositories/userRepository.js
@@ -1,18 +1,35 @@
 import sqlConfig from "../sqlConfig";
+import { getToken } from "../util";
 const sql = require("mssql");
 
-function getUsers(req, res) {
+function userLogin(req, res) {
   sql.connect(sqlConfig, (err) => {
     if (err) console.log(err);
-    var request = new sql.Request();
-    console.log(req);
-    request.query("select * from [User]", (err, data) => {
+    let request = new sql.Request();
+    let email = req.body.email;
+    let password = req.body.password;
+    let queryString = `select * from [User] where Email = '${email}' and Password = '${password}'`;
+    console.log(queryString);
+    request.query(queryString, (err, data) => {
       if (err) console.log(err);
-      res.send(data.recordset);
+      if (data.recordset.length > 0) {
+        let signinUser = data.recordset[0];
+        res.send({
+          User_ID: signinUser.User_ID,
+          FirstName: signinUser.FirstName,
+          LastName: signinUser.LastName,
+          Email: signinUser.Email,
+          City: signinUser.City,
+          Phone: signinUser.Phone,
+          token: getToken(signinUser),
+        });
+      } else {
+        res.status(401).send({ message: "Invalid Email or Password." });
+      }
     });
   });
 }
 
 module.exports = {
-  getUsers,
+  userLogin,
 };
